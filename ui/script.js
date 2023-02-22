cb3ab560e492786d7d01976e6fdbf422796676ec
Bastian Seidl <bastian.seidl@hotmail.com>
2023-02-22 23:31:10 +0100

    next button and multiple files

--- ui/script.js
+++ ui/script.js
@@ -1,11 +1,35 @@
 let selectFile = document.getElementById('selectFile');
-let file = null;
+let nextButton = document.getElementById('next');
+//let file = null;
+let audio = new Audio();
+let fileList = [];
+let count = 0;
+
+nextButton.addEventListener('click', () => {
+    //audio.stop()
+    audio.pause();
+    audio.currentTime = 0;
+    count++;
+    audio = new Audio(fileList[count].path);
+    audio.addEventListener("timeupdate", (event) => {
+        console.log(audio.currentTime);
+    })
+    audio.play();
+});
 
 selectFile.addEventListener('change', (event) => {
-    file = event.target.files[0];
+    //event.target.files.forEach(add);
+    //fileList.concat(event.target.files);
+    //file = event.target.files[0];
+    for(var i = 0; i < event.target.files.length; i++)
+    {
+        fileList.push(event.target.files[i]);
+    }
     console.log(event.target.files);
+    console.log(fileList);
+    count = 0;
 
-    let audio = new Audio(file.path);
+    audio = new Audio(fileList[count].path);
     audio.play();
 });
 
